3adcca37a53626d7e995124153e943dd4c082cba
Justin Klüver <justin.k@justplayer.de>
2020-08-04 13:45:48 +0200

    Disallow noclip lol

--- lua/boredomdotbat/modules/test.lua
+++ lua/boredomdotbat/modules/test.lua
@@ -4,11 +4,22 @@ Module.Description = "?"
 Module.CanEnable = function() return true end
 
 Module.OnEnable = function()
-    print("Test Enabled")
+    BoredomDotbat.LogDebug("Test Enabled")
+
+    -- Add limitation 
+    local function disallow(_, enable)
+        if enable then return false end
+    end
+
+    hook.Add("PlayerNoClip", Module.Name, disallow)
+    BoredomDotbat.HookIf(sam, "PlayerNoClip", "SAM.CanNoClip", disallow)
 end
 
 Module.OnDisable = function()
-    print("Test Disabled")
+    BoredomDotbat.LogDebug("Test Disabled")
+    -- Undo 
+    BoredomDotbat.HookUndo("PlayerNoClip", "SAM.CanNoClip")
+    hook.Remove("PlayerNoClip", Module.Name)
 end
 
 return Module
