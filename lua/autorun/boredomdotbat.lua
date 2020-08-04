b2ee0f837dc9683b527ccf598f906c3f10fbe3f3
Justin Klüver <justin.k@justplayer.de>
2020-08-04 12:18:50 +0200

    More Init stuff

--- lua/autorun/boredomdotbat.lua
+++ lua/autorun/boredomdotbat.lua
@@ -1,1 +1,10 @@
-print("Boring")
+print("Loading BoredomDotbat...")
+AddCSLuaFile("boredomdotbat/shared.lua")
+AddCSLuaFile("boredomdotbat/client.lua")
+include("boredomdotbat/shared.lua")
+
+if SERVER then
+    include("boredomdotbat/server.lua")
+else
+    include("boredomdotbat/client.lua")
+end
