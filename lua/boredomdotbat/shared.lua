2009dfec3fca05297ba9e403d11e69f44038abee
Justin Klüver <justin.k@justplayer.de>
2020-08-04 13:45:38 +0200

    Usefull hook functions

--- lua/boredomdotbat/shared.lua
+++ lua/boredomdotbat/shared.lua
@@ -1,1 +1,18 @@
 print("shared lol")
+local Hooks = {}
+
+function BoredomDotbat.HookIf(case, hookName, name, func)
+    BoredomDotbat.LogDebug("Overwrite hook: " .. hookName .. ":" .. name)
+    Hooks[hookName .. ":" .. name] = hook.GetTable()[hookName][name]
+    PrintTable(Hooks)
+
+    if isfunction(case) and case() or case then
+        hook.Add(hookName, name, func)
+    end
+end
+
+function BoredomDotbat.HookUndo(hookName, name)
+    BoredomDotbat.LogDebug("Undo hook: " .. hookName .. ":" .. name)
+    if not Hooks[hookName .. ":" .. name] then return end
+    hook.Add(hookName, name, Hooks[hookName .. ":" .. name])
+end
